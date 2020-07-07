5181e0c64f3edfed6265a4b38ba4e462b0bb9aab
Haven King <haven@hephaestus.dev>
2020-07-06 20:03:58 -0700

    Line of sight detection is now A-. Doesn't go through non-opaque blocks and needs to be sped up, but is great functionally.
    Mostly pushing so I can get back to working on my real PC.

--- src/main/java/dev/hephaestus/sax/mixin/server/DeObfuscateTargetedBlock.java
+++ src/main/java/dev/hephaestus/sax/mixin/server/DeObfuscateTargetedBlock.java
@@ -1,117 +1,28 @@
 package dev.hephaestus.sax.mixin.server;
 
 import com.mojang.authlib.GameProfile;
-import dev.hephaestus.sax.SAX;
-import dev.hephaestus.sax.world.OreCountRegistry;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket;
-import net.minecraft.server.network.ServerPlayNetworkHandler;
+import dev.hephaestus.sax.server.DeObfuscator;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.RayTraceContext;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.HashSet;
-import java.util.function.Consumer;
-
 @Mixin(ServerPlayerEntity.class)
-public abstract class DeObfuscateTargetedBlock extends PlayerEntity {
-    public DeObfuscateTargetedBlock(World world, BlockPos blockPos, GameProfile gameProfile) {
-        super(world, blockPos, gameProfile);
+public abstract class DeObfuscateTargetedBlock {
+    @Unique private DeObfuscator deObfuscator;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void initializeDeObfuscator(MinecraftServer server, ServerWorld world, GameProfile profile, ServerPlayerInteractionManager interactionManager, CallbackInfo ci) {
+        this.deObfuscator = new DeObfuscator((ServerPlayerEntity) (Object) this);
     }
-
-    @Shadow public abstract ServerWorld getServerWorld();
-
-    @Shadow public ServerPlayNetworkHandler networkHandler;
-    @Shadow public abstract boolean isDisconnected();
-
-    @Unique private BlockPos targetedBlock = null;
-
-    private static final int SEARCH_RADIUS = 30;
-
-    @Unique private final HashSet<BlockPos> revealed = new HashSet<>();
 
     @Inject(method = "playerTick", at = @At("TAIL"))
-    private void updateTargetedBlock(CallbackInfo ci) {
-//        if (!this.isDisconnected() && SoftBanManager.of(this.getServerWorld().getLevelProperties()).isBanned((ServerPlayerEntity) (Object) this)) {
-//            BlockHitResult hitResult = traceForBlock((ServerPlayerEntity) (Object) this, 5);
-//
-//            if (hitResult != null && hitResult.getBlockPos() != this.targetedBlock) {
-//                // Send them the fibbed version of the block they were previously looking at.
-//                if (this.targetedBlock != null) {
-//                    this.sendAroundTargetedBlocks(this.networkHandler::sendPacket);
-//                }
-//
-//                // Send them the non-fibbed version of the block they're looking at.
-//                this.targetedBlock = hitResult.getBlockPos();
-//                this.sendAroundTargetedBlocks(this.networkHandler.connection::send);
-//            }
-//        }
-
-        BlockPos blockPos = new BlockPos(this.getCameraPosVec(1F));
-
-        for (BlockPos pos : this.revealed) {
-            if (!pos.isWithinDistance(blockPos, SEARCH_RADIUS)) {
-                this.sendBlockUpdate(pos, this.networkHandler::sendPacket);
-            }
-        }
-
-        this.revealed.removeIf(pos -> !pos.isWithinDistance(blockPos, SEARCH_RADIUS));
-        BlockPos.Mutable mutable = new BlockPos.Mutable();
-
-        for (int x = -SEARCH_RADIUS; x <= SEARCH_RADIUS; ++x) {
-            for (int y = -SEARCH_RADIUS; y <= SEARCH_RADIUS; ++y) {
-                for (int z = -SEARCH_RADIUS; z <= SEARCH_RADIUS; ++z) {
-                    mutable.set(blockPos);
-                    mutable.move(x, y, z);
-
-                    if (mutable.isWithinDistance(blockPos, SEARCH_RADIUS) && OreCountRegistry.isTracked(this.world.getBlockState(mutable).getBlock())) {
-                        BlockPos pos = mutable.toImmutable();
-
-                        if (!this.revealed.contains(pos)) {
-                            Vec3d blockCenter = new Vec3d(
-                                    pos.getX() + 0.5 /*+ 0.5 * Integer.compare(x, 0)*/,
-                                    pos.getY() + 0.5 /*+ 0.5 * Integer.compare(y, 0)*/,
-                                    pos.getZ() + 0.5 /*+ 0.5 * Integer.compare(z, 0)*/
-                            );
-                            BlockHitResult hitResult = traceForBlock(
-                                    (ServerPlayerEntity) (Object) this,
-                                    blockCenter
-                            );
-
-                            if (hitResult != null && hitResult.getBlockPos().isWithinDistance(blockCenter, 1)) {
-//                                SAX.LOG.info("HIT:  [{}, {}, {}]", x, y, z);
-//                                SAX.LOG.info("[{}, {}, {}]", Integer.compare(x, 0), Integer.compare(y, 0), Integer.compare(z, 0));
-                                this.revealed.add(pos);
-                                this.sendBlockUpdate(pos, this.networkHandler.connection::send);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private void sendBlockUpdate(BlockPos pos, Consumer<BlockUpdateS2CPacket> consumer) {
-        consumer.accept(new BlockUpdateS2CPacket(this.world, pos));
-    }
-
-    private static BlockHitResult traceForBlock(ServerPlayerEntity player, Vec3d target) {
-        RayTraceContext context = new RayTraceContext(
-                player.getCameraPosVec(1F),
-                target,
-                RayTraceContext.ShapeType.VISUAL, RayTraceContext.FluidHandling.NONE, player
-        );
-
-        return player.world.rayTrace(context);
+    private void tickDeObfuscator(CallbackInfo ci) {
+        this.deObfuscator.tick();
     }
 }
---
+++ src/main/java/dev/hephaestus/sax/server/DeObfuscator.java
@@ -0,0 +1,105 @@
+package dev.hephaestus.sax.server;
+
+import dev.hephaestus.sax.world.OreCountRegistry;
+import net.minecraft.block.BlockState;
+import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.RayTraceContext;
+
+import java.util.HashSet;
+import java.util.function.Consumer;
+
+public class DeObfuscator {
+    private static final byte SEARCH_RADIUS = 30;
+
+    private final ServerPlayerEntity player;
+    private final HashSet<Vec3i> revealed = new HashSet<>();
+
+    public DeObfuscator(ServerPlayerEntity player) {
+        this.player = player;
+    }
+
+    public void tick() {
+        Vec3d origin = this.player.getCameraPosVec(1F);
+
+        for (Vec3i pos : this.revealed) {
+            if (!pos.isWithinDistance(origin, SEARCH_RADIUS)) {
+                this.sendBlockUpdate(new BlockPos(pos), this.player.networkHandler::sendPacket);
+            }
+        }
+
+        this.revealed.removeIf(pos -> !pos.isWithinDistance(origin, SEARCH_RADIUS));
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+
+        for (byte x = -SEARCH_RADIUS; x <= SEARCH_RADIUS; ++x) {
+            for (byte y = -SEARCH_RADIUS; y <= SEARCH_RADIUS; ++y) {
+                for (byte z = -SEARCH_RADIUS; z <= SEARCH_RADIUS; ++z) {
+                    if (x * x + y * y + z * z <= SEARCH_RADIUS * SEARCH_RADIUS) {
+                        mutable.set(origin.x, origin.y, origin.z);
+                        mutable.move(x, y, z);
+
+                        if (OreCountRegistry.isTracked(this.player.world.getBlockState(mutable).getBlock())) {
+                            Vec3i pos = mutable.toImmutable();
+
+                            if (!this.revealed.contains(pos)) {
+                                if (this.traceForBlock(this.player, pos)) {
+                                    this.revealed.add(pos);
+                                    this.sendBlockUpdate(new BlockPos(pos), this.player.networkHandler.connection::send);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void sendBlockUpdate(BlockPos pos, Consumer<BlockUpdateS2CPacket> consumer) {
+        if (this.player.getServer() != null) {
+            this.player.getServer().execute(() -> consumer.accept(new BlockUpdateS2CPacket(this.player.world, pos)));
+        }
+    }
+
+    private boolean traceForBlock(ServerPlayerEntity player, Vec3i target) {
+        boolean result = false;
+
+        for (byte dX = 0; dX <= 1; ++dX) {
+            for (byte dY = 0; dY <= 1; ++dY) {
+                for (byte dZ = 0; dZ <= 1; ++dZ) {
+                    Vec3d pos = new Vec3d(target.getX() + dX, target.getY() + dY, target.getZ() + dZ);
+                        RayTraceContext context = new RayTraceContext(
+                                player.getCameraPosVec(1F),
+                                pos,
+                                RayTraceContext.ShapeType.VISUAL, RayTraceContext.FluidHandling.NONE, player
+                        );
+
+                        BlockHitResult hitResult = rayTrace(context);
+                        result |= hitResult.getPos().isInRange(pos, 1.1F);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private BlockHitResult rayTrace(RayTraceContext context) {
+        BlockView blockView = this.player.world;
+        return BlockView.rayTrace(context, (rayTraceContext, blockPos) -> {
+            BlockState blockState = blockView.getBlockState(blockPos);
+            Vec3d vec3d = rayTraceContext.getStart();
+            Vec3d vec3d2 = rayTraceContext.getEnd();
+            VoxelShape voxelShape = rayTraceContext.getBlockShape(blockState, blockView, blockPos);
+            return blockView.rayTraceBlock(vec3d, vec3d2, blockPos, voxelShape, blockState);
+        }, (rayTraceContext) -> {
+            Vec3d vec3d = rayTraceContext.getStart().subtract(rayTraceContext.getEnd());
+            return BlockHitResult.createMissed(rayTraceContext.getEnd(), Direction.getFacing(vec3d.x, vec3d.y, vec3d.z), new BlockPos(rayTraceContext.getEnd()));
+        });
+    }
+}
